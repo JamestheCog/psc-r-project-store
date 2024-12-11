a6ba490e11303a71935728e01ae9bc6e72a79cd7
JamestheCog <jcog196013@gmail.com>
2024-12-11 09:54:09 +0800

    Registered application routes
    Also called load_dotenv()

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from dotenv import load_dotenv
 
 # Import our routes:
+from routes.api_routes import api_routes
+from routes.misc import misc
 
-app = Flask(__name__)
-app.register_blueprint()
+app = Flask(__name__) ; load_dotenv()
+app.register_blueprint(api_routes)
+app.register_blueprint(misc)
