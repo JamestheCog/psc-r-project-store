2b522dd382352706e77c1d6a8072ecebda229b25
JamestheCog <jcog196013@gmail.com>
2024-12-12 16:08:35 +0800

    Changed default second offset

--- utils/funcs.py
+++ utils/funcs.py
@@ -5,7 +5,7 @@ A Python helper file to store helper functions.
 from datetime import timedelta
 from dateutil.parser import isoparse
 
-def offset_datetime(timeset_object, second_offset = 1):
+def offset_datetime(timeset_object, second_offset = 30):
     '''
     Generates a datetime object in the form of form.gov.sg's 
     datetime object.  This will be used to fetch the data
