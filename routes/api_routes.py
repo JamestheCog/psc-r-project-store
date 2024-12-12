b8daf6502c5a6772c6ba3e3ea05b0c10ecd10f96
JamestheCog <jcog196013@gmail.com>
2024-12-12 12:46:43 +0800

    Return a single row
    for debugging purposes

--- routes/api_routes.py
+++ routes/api_routes.py
@@ -55,7 +55,7 @@ def fetch_data():
             data_to_share = shared_data
             if len(data_to_share) <= 1: 
                 break
-            db_info.append(data_to_share) ; beginning_timestamp = offset_datetime(data_to_share.get('timestamp'))
+            db_info.append(data_to_share) ; break # beginning_timestamp = offset_datetime(data_to_share.get('timestamp'))
         return(jsonify({'result' : db_info, 'status' : 200}), 200)
     except Exception as e:
         return(jsonify({'error_message' : str(e), 'status' : 500}), 500)
