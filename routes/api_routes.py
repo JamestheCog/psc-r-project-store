df37198bebcd7fa804f9cfa859e18fe2bdd715e0
JamestheCog <jcog196013@gmail.com>
2024-12-12 16:57:00 +0800

    Changed data fetching mechanism

--- routes/api_routes.py
+++ routes/api_routes.py
@@ -56,7 +56,7 @@ def fetch_data():
             print(data_to_share)
             if len(data_to_share) <= 1 or data_to_share is None: 
                 break
-            db_info.append(data_to_share) ; beginning_timestamp = offset_datetime(data_to_share.get('timestamp'))
+            db_info.append(data_to_share) ; beginning_timestamp = data_to_share.get('timestamp')
             print(beginning_timestamp)
         return(jsonify({'result' : db_info, 'status' : 200}), 200)
     except Exception as e:
