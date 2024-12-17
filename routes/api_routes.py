31126d69362ad43299dc984bb345e40f1d837823
JamestheCog <jcog196013@gmail.com>
2024-12-17 16:36:16 +0800

    Removed finally statemetns

--- routes/api_routes.py
+++ routes/api_routes.py
@@ -63,6 +63,7 @@ def upload():
     '''
     try:
         data = request.get_json()
+        print(data)
         if data['authorization'] is None:
             return(jsonify({'message' : 'Missing authorization information', 'status' : 400}), 400)
         if data['authorization']['password'] != os.getenv('PASSWORD'): 
