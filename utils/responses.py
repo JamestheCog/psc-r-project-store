bcc02e3d44b64b14546ccf874834c3a705f89a9f
JamestheCog <jcog196013@gmail.com>
2025-03-28 10:29:06 +0800

    Added helpers for encoding raw form responses
    I only did this for the health goals and the EQ5D5L stuffs

---
+++ utils/responses.py
@@ -0,0 +1,29 @@
+'''
+A module that contains helper functions to process raw questionnaire responses.  
+'''
+
+import json, os
+from cryptography.fernet import Fernet
+
+def process_eq5d5l(raw, fernet_key = os.environ.get('FERNET_KEY')):
+    '''
+    Maps a person's EQ5D5L response to a numeric grade as defined by the mapping dictionary. 
+    '''
+    decryptor, raw = Fernet(rf'{fernet_key}'), raw.lower()
+    with open('../resources/mappings/eqd5d5l_mappings.txt', 'rb') as file:
+        mapping_dictionary = json.loads(decryptor.decrypt(file.read()).decode('utf-8'))
+    for i in mapping_dictionary:
+        is_inside = list(map(lambda x : raw.startswith(x), mapping_dictionary[i]))
+        if sum(is_inside) > 0:
+            return(i)
+    return('-')
+
+def process_health_goals(raw_goals, fernet_key = os.getenv('FERNET_KEY')):
+    '''
+    Given a patient's health goals, encode it.
+    '''
+    raw_goals, decryptor = list(map(lambda x : x.lower(), raw_goals)), Fernet(fernet_key)
+    with open('../resources/mappings/health_goals.txt', 'rb') as encrypted:
+        health_goals = json.loads(decryptor.decrypt(encrypted.read()).decode('utf-8'))
+    conversions = [health_goals.get(i, '') for i in raw_goals]
+    return(', '.join(list(filter(lambda x : len(x.strip()) > 0, conversions))))
