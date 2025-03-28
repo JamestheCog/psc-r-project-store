a02a79076af62396fdc35c4bd5dceb2602210931
JamestheCog <jcog196013@gmail.com>
2025-03-28 10:30:50 +0800

    Removed non-essential routes and altered processing methodology

--- routes/api_routes.py
+++ routes/api_routes.py
@@ -4,13 +4,14 @@ on Plumber.
 '''
 
 from flask import Blueprint, request, jsonify
+from datetime import datetime
 import os, json, sqlitecloud
 import formsg
 from formsg.exceptions import WebhookAuthenticateException
 
 # Then, import our user helpers here:
-from utils.database import get_mapping_table, determine_table_name
-from utils.data import process_form_responses
+from utils.database import determine_table_name
+from utils.data import process_form_inputs, process_respondent_data
 
 # Define our blueprint and routes here; also load in the production version of formsg's SDK too:
 api_routes = Blueprint('api_routes', __name__)
@@ -26,46 +27,19 @@ def main_form_uploads():
         sdk.webhooks.authenticate(
             request.headers["X-FormSG-Signature"], 'https://psc-r-project-store-a3d7.onrender.com/main_form_uploads'
         )
-        mapping_dictionary = get_mapping_table()
         decrypted = sdk.crypto.decrypt(os.getenv('INTERVIEW_FORMS_KEY'), posted_data['data']) ; print(decrypted)
-        decrypted = process_form_responses(decrypted['responses'], mapping_dictionary)
+        decrypted = process_form_inputs(decrypted['responses'])
+        decrypted = process_respondent_data(decrypted)
+        print(decrypted)
         
         # Upload the data here:
-        cursor, table_name = conn.cursor(), determine_table_name(decrypted['arm']) ; print(decrypted['arm'])
+        cursor, table_name = conn.cursor(), determine_table_name(decrypted['patient_arm'])
         cursor.execute(f"PRAGMA table_info({table_name})") ; table_columns = [i[1] for i in cursor.fetchall()]
-        to_upload = [j if len(j.strip()) else '-' for j in [decrypted.get(i, '?') for i in table_columns]]
+        to_upload = [j if len(j.strip()) else '-' for j in [decrypted.get(i, '') for i in table_columns]]
         print(f"INSERT INTO {table_name} ({', '.join(table_columns)}) VALUES ({', '.join(['?'] * len(to_upload))})")
         cursor.execute(f"INSERT INTO {table_name} ({', '.join(table_columns)}) VALUES ({', '.join(['?'] * len(to_upload))})", to_upload)
         conn.commit() ; conn.close()
         return(jsonify({'message' : 'The patient\'s data has been successfully uploaded!'}), 200)
-    except WebhookAuthenticateException as e:
-        print(e)
-        return(jsonify({'message' : 'Bah!  Unauthorized request!'}, 401))
-    except Exception as e:
-        print(e)
-        return(jsonify({'message' : 'Something bad happened...', 'error' : str(e)}), 500)
-
-@api_routes.route('/caregiver_response_upload', methods = ['POST'])
-def caregiver_response_upload():
-    '''
-    Use form.gov.sg's SDK to upload the data onto the database:
-    '''
-    try:
-        posted_data, conn = json.loads(request.data), sqlitecloud.connect(os.getenv('DATABASE_CONNECTOR'))
-        sdk.webhooks.authenticate(
-            request.headers["X-FormSG-Signature"], 'https://psc-r-project-store-a3d7.onrender.com/caregiver_response_upload'
-        )
-        mapping_dictionary = get_mapping_table()
-        decrypted = sdk.crypto.decrypt(os.getenv('CAREGIVER_FORM_KEY'), posted_data['data'])
-        decrypted = process_form_responses(decrypted['responses'], mapping_dictionary)
-        
-        # Upload the data here:
-        cursor = conn.cursor()
-        cursor.execute(f"PRAGMA table_info({os.getenv('CAREGIVER_TABLE')})") ; table_columns = [i[1] for i in cursor.fetchall()]
-        to_upload = [j if len(j.strip()) else '-' for j in [decrypted.get(i, '?') for i in table_columns]]
-        cursor.execute(f"INSERT INTO {os.getenv('CAREGIVER_TABLE')} ({', '.join(table_columns)}) VALUES ({', '.join(['?'] * len(to_upload))})", to_upload)
-        conn.commit() ; conn.close()
-        return(jsonify({'message' : 'The caregiver\'s data has been successfully uploaded!'}), 200)
     except WebhookAuthenticateException as e:
         print(e)
         return(jsonify({'message' : 'Bah!  Unauthorized request!'}, 401))
