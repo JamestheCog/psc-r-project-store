9d0d68ed8d161f99082186e43f6c7e48fb2668aa
JamestheCog <jcog196013@gmail.com>
2025-04-01 14:23:01 +0800

    More printing

--- utils/responses.py
+++ utils/responses.py
@@ -46,5 +46,6 @@ def process_must(must_responses):
     Process some of the patient's responses to the MUST survey:
     '''
     print(must_responses)
-    input = {k : v.lower().split('-->')[-1].strip() for k, v in must_responses.items()}
+    input = {k : v.lower().split('-->')[-1].strip()[-1] for k, v in must_responses.items()}
+    print(input)
     return(input)
